38dd466bfcf5f0e0014d098676f06f2e8a6253e4
michaeldamage <michaeldamagesteam@gmail.com>
2022-10-29 17:48:54 +0200

    Initial commit. Default files, necessary media

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <?php wp_head(); ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Wyjazd centralny Diecezji Bydgoskiej">
+    <meta name="keywords" content="SDM, ŚDM, Światowe dni młodzieży, Swiatowe dni mlodziezy, lizbona, sdm bydgoszcz, swiatowe dni mlodziezy bydgoszcz, sdm bydgoszcz lizbona">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Z Kujaw i Pomorza do Lizbony</title>
+</head>
+    <body>
+        <header>
+            headers        
+        </header>
